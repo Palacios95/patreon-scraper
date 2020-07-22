75989cc234ca1392ba6788990118dcc94d35a174
Alejandro Palacios <apala067@fiu.edu>
2020-07-22 17:58:20 -0400

    drop rows that do not have dollar reward tiers

--- patreon_data/sample_data_clean.py
+++ patreon_data/sample_data_clean.py
@@ -2,10 +2,22 @@ import pandas as pd
 import numpy as np
 import re
 
-patron_df = pd.read_json(r"output.json")
+patron_df = pd.read_json(r"patreon_data/output.json")
 
 patron_df.reward_tiers = patron_df.reward_tiers.apply(
-    lambda x: list(map(lambda y: re.sub("[^0-9.]", "", y), x)))
+    lambda x: list(map(lambda y: re.sub("[^0-9.]", "", y), x))
+)
+
+non_dollar_reward_tier_row_ids = []
+for index, row in patron_df.iterrows():
+    for reward_tier in row["reward_tiers"]:
+        if not re.match(
+            "^[$]?[+-]?[0-9]{1,3}(?:,?[0-9]{3})*(?:\.[0-9]{2})?$", reward_tier
+        ):
+            non_dollar_reward_tier_row_ids.append(index)
+            break
+
+patron_df.drop(patron_df.index[non_dollar_reward_tier_row_ids], inplace=True)
 
 # grab number of users without a creator_id
 no_cid = len(patron_df.loc[patron_df["creator_id"].str.contains("user?")].index)
@@ -29,26 +41,28 @@ patron_df = patron_df.reset_index(drop=True)
 # calc avg_reward TODO: Calc average reward as new column, currently 0
 bad_row_indicies = []
 for index, row in patron_df.iterrows():
-    for x in row['reward_tiers']:
+    for x in row["reward_tiers"]:
         try:
             x = float(x)
         except ValueError:
             bad_row_indicies.append(row.name)
 
 # Show number of rows before drop
-print('# of rows before drop \n', len(patron_df))
+print("# of rows before drop \n", len(patron_df))
 final_df = patron_df.drop(patron_df.index[bad_row_indicies])
 final_df = final_df.reset_index(drop=True)
-print('# of rows in final df \n', len(final_df))
+print("# of rows in final df \n", len(final_df))
 
-final_df['reward_tiers'] = final_df['reward_tiers'].apply(lambda x: list(map(float, x)))
-final_df['average_reward'] = final_df['reward_tiers'].apply(lambda x: round(np.mean(x), 2))
+final_df["reward_tiers"] = final_df["reward_tiers"].apply(lambda x: list(map(float, x)))
+final_df["average_reward"] = final_df["reward_tiers"].apply(
+    lambda x: round(np.mean(x), 2)
+)
 
 print(final_df)
 
 # calc adj_monthly_income- TODO: Replace Monthly Income with num_patrons * avg revward tier
-final_df['patron_count'] = final_df['patron_count'].apply(lambda x: list(map(float, x)))
-print(type(final_df['patron_count'][0]))
+final_df["patron_count"] = final_df["patron_count"].apply(lambda x: list(map(float, x)))
+print(type(final_df["patron_count"][0]))
 # final_df["adj_monthly_income"] = final_df["patron_count"] * final_df['average_reward']
 
 with pd.option_context("display.max_rows", 10, "display.max_columns", None):
